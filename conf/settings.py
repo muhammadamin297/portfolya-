4e4178829a566e30efe58dedbe49dd4984a17c57
Muhammadamin <xojanazarovasilbek2008@gmail.com>
2025-09-27 14:30:55 +0500

    vazifa

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-#wkd=u+$+lyq%0f6ew%^s#v3j&zy9us%cezpi-4(wsd-9cdraj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'portfolya-mm.onrender.com']
+
+
 
 
 # Application definition
